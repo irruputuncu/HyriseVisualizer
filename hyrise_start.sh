0844d3181e09a4b4d69fa110ce65efb72449b66d
Tim Specht <tim@tim-specht.de>
2013-05-21 17:23:05 +0200

    Added starting shell script

---
+++ hyrise_start.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+cd /home/pascal/Projects/Epic/HyriseVI/Hyrise
+export HYRISE_DB_PATH=`pwd`/test
+export LD_LIBRARY_PATH=./build/:$LD_LIBRARY_PATH
+./build/hyrise_server -l ./build/log.properties -p 5000
