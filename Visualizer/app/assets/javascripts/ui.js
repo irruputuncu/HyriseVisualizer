929681026deb462250c6ff9c3ca3884a1494f7c5
Tim Specht <tim@tim-specht.de>
2013-07-06 18:34:25 +0200

    tried to add sliding function

--- Visualizer/app/assets/javascripts/ui.js
+++ Visualizer/app/assets/javascripts/ui.js
@@ -18,9 +18,18 @@ $(document).ready(function () {
                 }
                 if (($(ui.draggable).data("type") < 2 && $(this).children('[data-id="2"]').length == 0) || ($(ui.draggable).data("type") == 2 && $(this).children('[data-id="1"]').length == 0 && $(this).children('[data-id="0"]').length == 0)) {  
                     $(this).append($(ui.draggable).clone());
-                    $('.valueRangeSlider').slider({min: function() { return $(this).data('min-value');}, 
-                                                    max: function() { return $(this).data('max-value');},
-                                                     step:1});
+                    $('.valueRangeSlider').slider({min: /*function() { return $(this).data('min-value');}*/0, 
+                                                    max: /*function() { return $(this).data('max-value');},*/ 100,
+                                                     range:true,
+                                                     values:function() { 
+                                                        var array = Array.new
+                                                        array[0] = /*$(this).data('min-value')*/ 20;
+                                                        array[1] = /*$(this).data('max-value')*/ 70;
+                                                        return array;
+                                                    },
+                                                     slide: function( event, ui ) {
+                                                        console.log( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+                                                      }});
                     reloadData();
                 } else {
                     alert('You can only add columns of either number or string type at the same time');
